aa858a357525b2f7ebbb75a5112c6f83e664f4f9
jonas <jonas@ci-47919-3.uolcorp.intranet>
2018-09-21 11:21:22 -0300

    Test

--- src/main/java/br/com/job/domain/Domain.java
+++ src/main/java/br/com/job/domain/Domain.java
@@ -2,9 +2,20 @@ package br.com.job.domain;
 
 import java.util.Date;
 
+import org.bson.types.ObjectId;
+
 public class Domain {
 
+	private ObjectId _id;
 	private Date insertDate;
+
+	public ObjectId get_id() {
+		return _id;
+	}
+
+	public void set_id(ObjectId _id) {
+		this._id = _id;
+	}
 
 	public Date getInsertDate() {
 		return insertDate;
